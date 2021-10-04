01120917f71a0802e6acdb12512ca664c9ba94c5
Andoni Sooklaris <andonis@apple.com>
2021-10-04 10:42:14 -0500

    Integrated versioneer

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 
 """The setup script."""
 
+import versioneer
 from setuptools import setup, find_packages
 
 with open('README.rst') as readme_file:
@@ -39,6 +40,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/tsouchlarakis/sql_query_tools',
-    version='0.1.0',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     zip_safe=False,
 )
